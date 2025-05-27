769ad88a15741fa892a43dc49aa6459d01c70bb5
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2025-05-27 21:14:47 +0300

    useRepository

--- src/main/java/com/beworking/auth/UserRepository.java
+++ src/main/java/com/beworking/auth/UserRepository.java
@@ -1,9 +1,7 @@
 package com.beworking.auth;
 
-import org.springframework.data.jpa.repository.JpaRepository; // it contains the JpaRepository interface. It is a part of Spring Data JPA and provides methods for CRUD operations and pagination.
-import java.util.Optional; // it is a container object which may or may not contain a non-null value. It is used to avoid null references and NullPointerExceptions.
-
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional; 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 } 
