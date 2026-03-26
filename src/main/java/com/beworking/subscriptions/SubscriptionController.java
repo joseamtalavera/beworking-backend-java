fe696d19a3590f7137acb4219bff7457938ee8ce
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2026-03-26 12:19:25 +0100

    Use requested start date as Stripe billing anchor instead of always next month

--- src/main/java/com/beworking/subscriptions/SubscriptionController.java
+++ src/main/java/com/beworking/subscriptions/SubscriptionController.java
@@ -206,15 +206,22 @@ public class SubscriptionController {
                 stripeRequest.put("vat_number", taxExempt ? resolvedVat : "");
                 stripeRequest.put("tax_exempt", taxExempt);
 
-                // Anchor billing to the 1st of next month
-                LocalDate firstOfNextMonth = LocalDate.now().plusMonths(1).withDayOfMonth(1);
-                long anchorEpoch = firstOfNextMonth.atStartOfDay().toEpochSecond(ZoneOffset.UTC);
-                stripeRequest.put("billing_cycle_anchor", anchorEpoch);
-
-                // If start date is on the 1st (future), skip proration; otherwise prorate from today
+                // Anchor billing to the requested start date
                 LocalDate startDate = request.getStartDate() != null ? request.getStartDate() : LocalDate.now();
-                boolean startsOnFirst = startDate.getDayOfMonth() == 1 && !startDate.isBefore(firstOfNextMonth);
-                stripeRequest.put("proration_behavior", startsOnFirst ? "none" : "create_prorations");
+                boolean isFuture = startDate.isAfter(LocalDate.now());
+
+                if (isFuture) {
+                    // Future start: use Stripe subscription schedules (handled by stripe-service)
+                    long anchorEpoch = startDate.atStartOfDay().toEpochSecond(ZoneOffset.UTC);
+                    stripeRequest.put("billing_cycle_anchor", anchorEpoch);
+                    stripeRequest.put("proration_behavior", "none");
+                } else {
+                    // Start now or in the past: anchor to 1st of next month, prorate
+                    LocalDate firstOfNextMonth = LocalDate.now().plusMonths(1).withDayOfMonth(1);
+                    long anchorEpoch = firstOfNextMonth.atStartOfDay().toEpochSecond(ZoneOffset.UTC);
+                    stripeRequest.put("billing_cycle_anchor", anchorEpoch);
+                    stripeRequest.put("proration_behavior", "create_prorations");
+                }
 
                 // Pass billing interval (month, quarter, year)
                 String interval = request.getBillingInterval() != null ? request.getBillingInterval() : "month";
